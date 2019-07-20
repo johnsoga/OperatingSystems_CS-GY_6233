5bfd24da848004824edf527cdab06e3d450c30b9
Gary Johnson <garycjohnso@gmail.com>
2019-07-20 03:43:35 -0400

    Lab #3

--- cpu.c
+++ cpu.c
@@ -121,7 +121,7 @@ struct PCB handle_process_completion_pp(struct PCB ready_queue[QUEUEMAX], int *q
         ready_queue[i] = ready_queue[i+1];
     }
 
-    //*queue_cnt+=1;
+    *queue_cnt-=1;
     ready_queue[max].execution_starttime = timestamp;
     ready_queue[max].execution_endtime = timestamp + ready_queue[max].remaining_bursttime;
     return ready_queue[max];

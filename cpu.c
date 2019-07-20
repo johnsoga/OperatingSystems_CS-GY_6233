a08b624810f617b04822f0892b0d40f8b3604a0c
Gary Johnson <garycjohnso@gmail.com>
2019-07-20 03:25:34 -0400

    Lab #3

--- cpu.c
+++ cpu.c
@@ -75,8 +75,8 @@ struct PCB handle_process_arrival_srtp(struct PCB ready_queue[QUEUEMAX], int *qu
 
             return current_process;
         } else {
-            new_process.execution_starttime = timestamp;
-            new_process.execution_endtime = timestamp + new_process.total_bursttime;
+            new_process.execution_starttime = time_stamp;
+            new_process.execution_endtime = time_stamp + new_process.total_bursttime;
             new_process.remaining_bursttime = new_process.total_bursttime;
             current_process.execution_starttime = 0;
             current_process.execution_endtime = 0;

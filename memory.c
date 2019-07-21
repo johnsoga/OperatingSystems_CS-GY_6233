c30832d3273933c6b905962e4761049cdabe108c
Gary Johnson <garycjohnso@gmail.com>
2019-07-21 02:09:15 -0400

    Lab #4

--- memory.c
+++ memory.c
@@ -46,3 +46,19 @@ struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memo
         return memory_map[best_fit_size];
     }
 }
+struct MEMORY_BLOCK first_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id) {
+
+    return struct MEMORY_BLOCK;
+}
+struct MEMORY_BLOCK next_fit_allocate(int request_size, struct MEMORY_BLOCK   memory_map[MAPMAX],int *map_cnt, int process_id, int last_address) {
+
+    return struct MEMORY_BLOCK;
+}
+struct MEMORY_BLOCK worst_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id) {
+
+    return struct MEMORY_BLOCK;
+}
+void release_memory(struct MEMORY_BLOCK freed_block, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt) {
+
+    return;
+}

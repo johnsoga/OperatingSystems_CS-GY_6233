dd6edc772ac2a268a87be4dc207a0195cec00b26
Gary Johnson <garycjohnso@gmail.com>
2019-07-22 03:13:57 -0400

    Lab #5

--- virtual.c
+++ virtual.c
@@ -86,11 +86,61 @@ int process_page_access_lfu(struct PTE page_table[TABLEMAX],int *table_cnt, int 
         }
     }
 }
-int count_page_faults_fifo(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt) {
-
-    return 0;
-}
 int process_page_access_lru(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp) {
+
+    int i, smallest_ref, smallest_ref_location, first_guess;
+
+    if(page_table[page_number].is_valid == 1) {
+        page_table[page_number].last_access_timestamp = current_timestamp;
+        page_table[page_number].reference_count +=1;
+
+        return page_table[page_number].frame_number;
+    } else {
+        if(*frame_cnt != 0) {
+            (*frame_cnt)--;
+            page_table[page_number].is_valid = 1;
+            page_table[page_number].frame_number = frame_pool[*frame_cnt];
+            page_table[page_number].arrival_timestamp = current_timestamp;
+            page_table[page_number].last_access_timestamp = current_timestamp;
+            page_table[page_number].reference_count = 1;
+
+            return page_table[page_number].frame_number;
+        } else {
+            first_guess = 1;
+            smallest_ref = 0;
+            for(i = 0; i < *table_cnt; i++) {
+                if(page_table[i].is_valid == 1) {
+                    if(first_guess) {
+                        smallest_ref = page_table[i].reference_count;
+                        smallest_ref_location = i;
+                        first_guess = 0;
+                    } else if(page_table[i].reference_count < smallest_ref) {
+                        smallest_ref = page_table[i].reference_count;
+                        smallest_ref_location = i;
+                    } else if(page_table[i].reference_count == smallest_ref) {
+                        if(page_table[i].arrival_timestamp < page_table[smallest_ref_location].arrival_timestamp) {
+                            smallest_ref = page_table[i].reference_count;
+                            smallest_ref_location = i;
+                        }
+                }
+            }
+
+            page_table[smallest_ref_location].is_valid = 0;
+            page_table[page_number].frame_number = page_table[smallest_ref_location].frame_number;
+            page_table[smallest_ref_location].frame_number = -1;
+            page_table[smallest_ref_location].arrival_timestamp = 0;
+            page_table[smallest_ref_location].last_access_timestamp = 0;
+            page_table[smallest_ref_location].reference_count = 0;
+            page_table[page_number].is_valid = 1;
+            page_table[page_number].arrival_timestamp = current_timestamp;
+            page_table[page_number].last_access_timestamp = current_timestamp;
+            page_table[page_number].reference_count = 1;
+
+            return page_table[page_number].frame_number;
+        }
+    }
+}
+int count_page_faults_fifo(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt) {
 
     return 0;
 }

999e2c7bcd9f6ee20328f860ba0c141fb3d00135
Gary Johnson <garycjohnso@gmail.com>
2019-07-23 23:27:54 -0400

    Lab #5

--- disk.c
+++ disk.c
@@ -20,3 +20,23 @@ struct RCB handle_request_arrival_fcfs(struct RCB request_queue[QUEUEMAX], int *
         return current_request;
     }
 }
+struct RCB handle_request_arrival_look(struct RCB request_queue[QUEUEMAX],int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp) {
+
+    return current_request;
+}
+struct RCB handle_request_arrival_sstf(struct RCB request_queue[QUEUEMAX],int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp) {
+
+    return current_request;
+}
+struct RCB handle_request_completion_fcfs(struct RCB request_queue[QUEUEMAX],int *queue_cnt) {
+
+    return request_queue[0];
+}
+struct RCB handle_request_completion_look(struct RCB request_queue[QUEUEMAX],int  *queue_cnt, int current_cylinder, int scan_direction) {
+
+    return request_queue[0];
+}
+struct RCB handle_request_completion_sstf(struct RCB request_queue[QUEUEMAX],int *queue_cnt,int current_cylinder) {
+
+    struct RCB handle_request_completion_sstf(struct RCB request_queue[QUEUEMAX],int *queue_cnt,int current_cylinder); 
+}
